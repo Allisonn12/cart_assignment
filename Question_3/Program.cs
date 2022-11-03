01074bf656d7fe42925597e6205b98524d42fc50
Allisonn12 <113454936+Allisonn12@users.noreply.github.com>
2022-11-03 21:29:07 +0200

    Update Program.cs

--- Question_3/Program.cs
+++ Question_3/Program.cs
@@ -1,4 +1,4 @@
-﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http;
 using RestSharp;
 using System;
 using System.Net;
@@ -18,60 +18,40 @@ namespace HttpClientSample
     {
         static HttpClient client = new HttpClient();
 
-        //static string user_first_name;
-        //static string user_last_name;
-        //static string user_email;
-        //static int user_phone_numer;
+        
 
-        static void ShowUser(userClass user)
-        {
-            Console.WriteLine($"Name: {user.first_name}\tLast Name: " +
-                $"{user.last_name}\tEmail: {user.email}" +
-                $"Phone Number: {user.phone_number}");
-        }
 
-        static async Task<Uri> CreateCardAsync(userClass name)
+        static async Task<Uri> CreateCardAsync(userClass name, string user_name, string user_lname, string user_email, int user_phone_number)
         {
             userClass cls = new userClass();
             HttpResponseMessage response = await client.PostAsJsonAsync(
-                 "https://api.trello.com/1/lists?name=Please_word&idBoard=6362271e643dc4008257dbe0&key=c739eb66ceb45a7b1cc5b7da913bef93&token=a53373ff08e73f72f8ae38bb54f482804b3c4ef8bb7a9d32b3654fe6d9fa109c", name);
+                 "https://api.trello.com/1/lists?name=New_card&idBoard=5a4b3c4cbe0188ca9c0b2058&key=6eb508bda626ff893db446eff50d0066&token=ae4a73cb0e40c46f6e642f5f7429394534b35e3b5a4c7c21438e5389eec20497", name);
 
+            user_name = cls.first_name;
+            user_lname = cls.last_name;
+            user_email = cls.email;
+            user_phone_number = cls.phone_number;
+
+            string phone = Convert.ToString(user_phone_number);
+            string[] arr = { user_name, user_lname, user_email, phone};
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                HttpResponseMessage resp = await client.PostAsJsonAsync(
+                     "https://api.trello.com/1/cards?idList=634bfc7a3e039d0031f4dac4&key=6eb508bda626ff893db446eff50d0066&token=ae4a73cb0e40c46f6e642f5f7429394534b35e3b5a4c7c21438e5389eec20497", arr[i]);
+
+            }
+
+
+
+        
             response.EnsureSuccessStatusCode();
 
             // return URI of the created resource.
             return response.Headers.Location;
         }
 
-        static async Task<userClass> GetUserAsync(string path)
-        {
-            //path = "https://api.trello.com/1/cards/{id}?key=c739eb66ceb45a7b1cc5b7da913bef93&token=a53373ff08e73f72f8ae38bb54f482804b3c4ef8bb7a9d32b3654fe6d9fa109c";
-            userClass user = null;
-            HttpResponseMessage response = await client.GetAsync(path);
-            if (response.IsSuccessStatusCode)
-            {
-                user = await response.Content.ReadAsAsync<userClass>();
-            }
-            return user;
-        }
-
-        static async Task<userClass> UpdateUserAsync(userClass users)
-        {
-            HttpResponseMessage response = await client.PutAsJsonAsync(
-                $"https://api.trello.com/1/lists?name=Please_word&idBoard=6362271e643dc4008257dbe0&key=c739eb66ceb45a7b1cc5b7da913bef93&token=a53373ff08e73f72f8ae38bb54f482804b3c4ef8bb7a9d32b3654fe6d9fa109c", users);
-            response.EnsureSuccessStatusCode();
-
-            // Deserialize the updated user from the response body.
-            users = await response.Content.ReadAsAsync<userClass>();
-            return users;
-        }
-
-        static async Task<HttpStatusCode> DeleteUserAsync(string id)
-        {
-            HttpResponseMessage response = await client.DeleteAsync(
-                $"https://api.trello.com/1/lists?name=Please_word&idBoard=6362271e643dc4008257dbe0&key=c739eb66ceb45a7b1cc5b7da913bef93&token=a53373ff08e73f72f8ae38bb54f482804b3c4ef8bb7a9d32b3654fe6d9fa109c");
-            return response.StatusCode;
-        }
-
+        
         static void Main()
         {
             RunAsync().GetAwaiter().GetResult();
@@ -95,25 +75,15 @@ namespace HttpClientSample
                     phone_number = 0815215211
                 };
 
-                var url = await CreateCardAsync(users);
-                Console.WriteLine($"Created at {url}");
+                string userName = users.first_name;
+                string userLName = users.last_name ;
+                string userEmail = users.email;
+                int pNum = users.phone_number;
 
-                // Get the user
-                users = await GetUserAsync(url.PathAndQuery);
-                ShowUser(users);
+                var url = await CreateCardAsync(users, userName, userLName, userEmail, pNum);
+                Console.WriteLine($"Registration was successful");
 
-                // Update the user
-                Console.WriteLine("Updating user...");
-                users.phone_number = 80;
-                await UpdateUserAsync(users);
-
-                // Get the updated user
-                users = await GetUserAsync(url.PathAndQuery);
-                ShowUser(users);
-
-                // Delete the user
-                var statusCode = await DeleteUserAsync(users.id);
-                Console.WriteLine($"Deleted (HTTP Status = {(int)statusCode})");
+                
 
             }
             catch (Exception e)
