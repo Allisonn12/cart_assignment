69b60338ec715364242bf144c928ca3b5928e783
Allisonn12 <113454936+Allisonn12@users.noreply.github.com>
2022-11-04 10:53:59 +0200

    Update DAL.cs

--- Question_2/Models/DAL.cs
+++ Question_2/Models/DAL.cs
@@ -139,7 +139,8 @@ namespace carterAPI.Models
 
             return resp;
         }
-
+        
+        // Get car by ID
         public Response getCarById(SqlConnection con, int id)
         {
             Response resp = new Response();
@@ -178,7 +179,7 @@ namespace carterAPI.Models
             return resp;
         }
 
-
+        // Get All cars
         public Response getAllCars(SqlConnection con)
         {
             Response resp = new Response();
@@ -303,130 +304,43 @@ namespace carterAPI.Models
         }
 
 
-
-        // Get All purchases
+        // Get all purchases
         public Response getAllPurchases(SqlConnection con)
         {
             Response resp = new Response();
-            SqlDataAdapter da = new SqlDataAdapter("SELECT p.*, ci.*, c.* " +
-                "FROM tbl_purchases p " +
+            SqlDataAdapter da = new SqlDataAdapter("SELECT p.*, ci.*, c.* FROM tbl_purchases p " +
                 "LEFT JOIN tbl_customer_info ci ON ci.cust_id = p.cust_id " +
-                "INNER JOIN tbl_car_info c ON c.car_id = p.car_id", con);
+                "LEFT JOIN tbl_car_info c ON c.car_id = p.car_id", con);
             DataTable dt = new DataTable();
-            List<purchasesEndpoint> point = new List<purchasesEndpoint>();
+            Endpoint point = new Endpoint();
             da.Fill(dt);
             if (dt.Rows.Count > 0)
             {
-                for (int i = 0; i < dt.Rows.Count; i++)
-                {
-                    purchasesEndpoint list_point = new purchasesEndpoint();
-                    carEndpoint list_cpoint = new carEndpoint();
-                    Endpoint list_purchasepoint = new Endpoint();
-                    list_point.purchase_id = Convert.ToInt32(dt.Rows[i]["id"]);
-                    list_point.car_id = Convert.ToInt32(dt.Rows[i]["car_id"]);
-                    list_point.cust_id = Convert.ToInt32(dt.Rows[i]["cust_id"]);
-                    list_purchasepoint.cust_id = Convert.ToInt32(dt.Rows[0]["cust_id"]);
-                    list_purchasepoint.first_name = Convert.ToString(dt.Rows[0]["first_name"]);
-                    list_purchasepoint.last_name = Convert.ToString(dt.Rows[0]["last_name"]);
-                    list_purchasepoint.phone_number = Convert.ToString(dt.Rows[0]["phone_number"]);
-                    list_cpoint.car_id = Convert.ToInt32(dt.Rows[0]["car_id"]);
-                    list_cpoint.car_name = Convert.ToString(dt.Rows[0]["car_name"]);
-                    list_cpoint.car_model = Convert.ToString(dt.Rows[0]["car_model"]);
-                    list_cpoint.car_image = Convert.ToString(dt.Rows[0]["car_image"]);
-                    list_cpoint.year_released = Convert.ToInt32(dt.Rows[0]["year_released"]);
-                    list_cpoint.no_of_liters = Convert.ToDecimal(dt.Rows[0]["no_of_liters"]);
-                    list_cpoint.transmission = Convert.ToString(dt.Rows[0]["transmission"]);
-                    list_cpoint.price = Convert.ToDecimal(dt.Rows[0]["price"]);
-                    list_cpoint.monthly_installment = Convert.ToDecimal(dt.Rows[0]["monthly_installment"]);
-                    list_cpoint.insurance = Convert.ToInt32(dt.Rows[0]["insurance"]);
-                    list_cpoint.warrenty = Convert.ToInt32(dt.Rows[0]["warrenty"]);
-                    list_cpoint.good_features = Convert.ToString(dt.Rows[0]["good_features"]);
-                    list_cpoint.bad_features = Convert.ToString(dt.Rows[0]["bad_features"]);
-
-
-                    point.Add(list_point);
-                }
-            }
-
-            if (point.Count > 0)
-            {
+                Endpoint poi = new Endpoint();
+                carEndpoint list_point = new carEndpoint();
+                carEndpoint car = new carEndpoint();
+                poi.cust_id = Convert.ToInt32(dt.Rows[0]["cust_id"]);
+                poi.first_name = Convert.ToString(dt.Rows[0]["first_name"]);
+                poi.last_name = Convert.ToString(dt.Rows[0]["last_name"]);
+                poi.email = Convert.ToString(dt.Rows[0]["email"]);
+                poi.phone_number = Convert.ToString(dt.Rows[0]["phone_number"]);
+                list_point.car_id = Convert.ToInt32(dt.Rows[0]["car_id"]);
+                list_point.car_name = Convert.ToString(dt.Rows[0]["car_name"]);
+                list_point.car_model = Convert.ToString(dt.Rows[0]["car_model"]);
+                list_point.car_image = Convert.ToString(dt.Rows[0]["car_image"]);
+                list_point.year_released = Convert.ToInt32(dt.Rows[0]["year_released"]);
+                list_point.no_of_liters = Convert.ToInt32(dt.Rows[0]["no_of_liters"]);
+                list_point.transmission = Convert.ToString(dt.Rows[0]["transmission"]);
+                list_point.price = Convert.ToDecimal(dt.Rows[0]["price"]);
+                list_point.monthly_installment = Convert.ToDecimal(dt.Rows[0]["monthly_installment"]);
+                list_point.insurance = Convert.ToInt32(dt.Rows[0]["insurance"]);
+                list_point.warrenty = Convert.ToInt32(dt.Rows[0]["warrenty"]);
+                list_point.good_features = Convert.ToString(dt.Rows[0]["good_features"]);
+                list_point.bad_features = Convert.ToString(dt.Rows[0]["bad_features"]);
                 resp.StatusCode = 200;
                 resp.StatusMessage = "Data found";
-                resp.purchase_Endpoint = point;
-            }
-            else
-            {
-                resp.StatusCode = 404;
-                resp.StatusMessage = "Data not found";
-                resp.list_point = null;
-            }
-
-
-            return resp;
-        }
-
-        //public Response getPurchase(SqlConnection con)
-        //{
-        //    Response resp = new Response();
-        //    SqlDataAdapter da = new SqlDataAdapter("SELECT * FROM tbl_purchases", con);
-        //    DataTable dt = new DataTable();
-        //    List<purchasesEndpoint> point = new List<purchasesEndpoint>();
-        //    da.Fill(dt);
-        //    if (dt.Rows.Count > 0)
-        //    {
-        //        for (int i = 0; i < dt.Rows.Count; i++)
-        //        {
-        //            purchasesEndpoint list_point = new purchasesEndpoint();
-        //            list_point.purchase_id = Convert.ToInt32(dt.Rows[i]["id"]);
-        //            list_point.cust_id = Convert.ToInt32(dt.Rows[i]["cust_id"]);
-        //            list_point.car_id = Convert.ToInt32(dt.Rows[i]["car_id"]);
-
-        //            point.Add(list_point);
-        //        }
-        //    }
-
-        //    if (point.Count > 0)
-        //    {
-        //        resp.StatusCode = 200;
-        //        resp.StatusMessage = "Data found";
-        //        resp.purchase_Endpoint = point;
-        //    }
-        //    else
-        //    {
-        //        resp.StatusCode = 404;
-        //        resp.StatusMessage = "Data not found";
-        //        resp.list_point = null;
-        //    }
-
-
-        //    return resp;
-        //}
-
-        public Response storedProcedure(SqlConnection con, int dealer_id)
-        {
-            Random random = new Random();
-            int ran = random.Next(999999);
-            Response resp = new Response();
-            SqlDataAdapter da = new SqlDataAdapter("CREATE PROCEDURE getDealership"+ran+
-                " @DealerID nvarchar(255) AS " +
-                "SELECT test_deler.Dealership_Name, dealer_sales.SaleAmount " +
-                "FROM SQL_Test_Dealers test_deler " +
-                "INNER JOIN SQL_Test_Dealer_Sales dealer_sales ON test_deler.DealerID = dealer_sales.DealerID " +
-                "WHERE test_deler.DealerID = @DealerID; " +
-                "EXEC getDealership"+ran+" @DealerID = '"+dealer_id+"' ", con);
-            DataTable dt = new DataTable();
-            storedProcedure point = new storedProcedure();
-            da.Fill(dt);
-            if (dt.Rows.Count > 0)
-            {
-                storedProcedure list_point = new storedProcedure();
-                list_point.DealerID = Convert.ToInt32(dt.Rows[0]["DealerID"]);
-                list_point.DateOfSale = Convert.ToDateTime(dt.Rows[0]["DateOfSale"]);
-                list_point.SalesAmount = Convert.ToDecimal(dt.Rows[0]["SalesAmount"]);
-                list_point.Dealership_name = Convert.ToString(dt.Rows[0]["Dealership_Name"]);
-                resp.StatusCode = 200;
-                resp.StatusMessage = "Data found";
-                resp.storedProcedure = list_point;
+                resp.car_point = list_point;
+                resp.end_point = poi;
             }
             else
             {
@@ -438,6 +352,45 @@ namespace carterAPI.Models
 
             return resp;
         }
+
+        
+        // 
+//         public Response storedProcedure(SqlConnection con, int dealer_id)
+//         {
+//             Random random = new Random();
+//             int ran = random.Next(999999);
+//             Response resp = new Response();
+//             SqlDataAdapter da = new SqlDataAdapter("CREATE PROCEDURE getDealership"+ran+
+//                 " @DealerID nvarchar(255) AS " +
+//                 "SELECT test_deler.Dealership_Name, dealer_sales.SaleAmount " +
+//                 "FROM SQL_Test_Dealers test_deler " +
+//                 "INNER JOIN SQL_Test_Dealer_Sales dealer_sales ON test_deler.DealerID = dealer_sales.DealerID " +
+//                 "WHERE test_deler.DealerID = @DealerID; " +
+//                 "EXEC getDealership"+ran+" @DealerID = '"+dealer_id+"' ", con);
+//             DataTable dt = new DataTable();
+//             storedProcedure point = new storedProcedure();
+//             da.Fill(dt);
+//             if (dt.Rows.Count > 0)
+//             {
+//                 storedProcedure list_point = new storedProcedure();
+//                 list_point.DealerID = Convert.ToInt32(dt.Rows[0]["DealerID"]);
+//                 list_point.DateOfSale = Convert.ToDateTime(dt.Rows[0]["DateOfSale"]);
+//                 list_point.SalesAmount = Convert.ToDecimal(dt.Rows[0]["SalesAmount"]);
+//                 list_point.Dealership_name = Convert.ToString(dt.Rows[0]["Dealership_Name"]);
+//                 resp.StatusCode = 200;
+//                 resp.StatusMessage = "Data found";
+//                 resp.storedProcedure = list_point;
+//             }
+//             else
+//             {
+//                 resp.StatusCode = 404;
+//                 resp.StatusMessage = "Data not found";
+//                 resp.end_point = null;
+//             }
+
+
+//             return resp;
+//         }
 
 
     }
